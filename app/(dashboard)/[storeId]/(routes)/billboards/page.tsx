672c638162901e4fc7b0ee6a1fb9408b62ff871c
chineduCoded <chinedujohn17@gmail.com>
2023-12-06 00:35:58 +0100

    Implement billboard API routes

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -0,0 +1,14 @@
+import { BillboardClient } from "./components/client"
+
+
+const BillboardsPage = () => {
+  return (
+    <div className="flex-col">
+        <div className="flex-1 p-8 space-y-4 pt-6">
+            <BillboardClient />
+        </div>
+    </div>
+  )
+}
+
+export default BillboardsPage
